180a4dfbebdc6395e57bc6b0be8a000ff0156955
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-09-29 20:19:29 +0530

    tweeks

--- week-01/Patterns/07_Javascript_Pyramid_Pattern.js
+++ week-01/Patterns/07_Javascript_Pyramid_Pattern.js
@@ -9,19 +9,23 @@
 */
 
 
-function pyramidPattern(n){
+function pyramidPattern(n,star){
     let str ='';
-    for(let row = 0;row<n;row++){
-        for(let col = 0;col<=n+row; col++){
+    for(let row = 0;!star?row<n:row<n-1;row++){
+        for(let col = 1;col<=n+row; col++){
             if(col <n-row){
                 str+=' '
             }else{
                 str+='*'
             }
         }
-        str+='\n';
+        if(!star?row!==n-1:row!==n-2){
+            str+='\n';
+        }
     }
     console.log(str);
 }
 
 pyramidPattern(5);
+
+export default pyramidPattern
