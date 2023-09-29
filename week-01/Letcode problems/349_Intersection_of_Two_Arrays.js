18c7a37bec45a7751acb5bce37d33dc83571cafa
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-09-29 22:28:06 +0530

    added leetcode problem of arrays intersection

---
+++ week-01/Letcode problems/349_Intersection_of_Two_Arrays.js
@@ -0,0 +1,41 @@
+/*
+349. Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const ArraysIntersection = (nums1, nums2) => {
+    const set1 = new Set(nums1);
+    const result = [];
+
+    for (const num of nums2) {
+        if (set1.has(num)) {
+            result.push(num);
+            set1.delete(num); // Remove the element from set1 to avoid duplicates
+        }
+    }
+
+    return result;
+};
+
+
+console.log(ArraysIntersection([1,2,2,1],[2,2]))
+
