78d4c79859df24443d26aa5b05cf72e86e47487f
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-10-07 22:30:43 +0530

    implemented cors

--- week-03/02-nodejs/solutions/todoServer.solution.file.js
+++ week-03/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
+var cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.use(bodyParser.json());
-
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) return i;
@@ -103,4 +104,5 @@ app.use((req, res, next) => {
   res.status(404).send();
 });
 
+app.listen(3000)
 module.exports = app;
