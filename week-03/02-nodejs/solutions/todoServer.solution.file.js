004727f90da224c6d702dfbc888b5fdd957d19a7
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-10-07 22:58:38 +0530

    fix

--- week-03/02-nodejs/solutions/todoServer.solution.file.js
+++ week-03/02-nodejs/solutions/todoServer.solution.file.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
+var path = require('path')
 var cors = require('cors')
 
 const app = express();
+app.use(bodyParser.json());
 app.use(cors())
 
-app.use(bodyParser.json());
 function findIndex(arr, id) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].id === id) return i;
@@ -21,6 +22,10 @@ function removeAtIndex(arr, index) {
   }
   return newArray;
 }
+
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname, '../../todo-app-frontend/index.html'));
+})
 
 app.get('/todos', (req, res) => {
   fs.readFile("todos.json", "utf8", (err, data) => {
@@ -43,6 +48,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.post('/todos', (req, res) => {
+  console.log(req.body)
   const newTodo = {
     id: Math.floor(Math.random() * 1000000), // unique random id
     title: req.body.title,
@@ -103,6 +109,8 @@ app.delete('/todos/:id', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send();
 });
+
+
 
 app.listen(3000)
 module.exports = app;
