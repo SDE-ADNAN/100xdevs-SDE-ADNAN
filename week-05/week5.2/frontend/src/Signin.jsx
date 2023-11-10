1c2cd9e279a59a1c6c2e5ac0d3438877f4ab8417
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-11-10 23:37:58 +0530

    new signin component

--- week-05/week5.2/frontend/src/Signin.jsx
+++ week-05/week5.2/frontend/src/Signin.jsx
@@ -1,38 +1,70 @@
 import Button from '@mui/material/Button';
 import TextField from "@mui/material/TextField";
-import {Card, Typography} from "@mui/material";
+import { Card, Typography } from "@mui/material";
+import { useState } from 'react';
 
 function Signin() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSignIn = async () => {
+        try {
+            const response = await fetch("http://localhost:3000/admin/login", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: email,
+                    password: password
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            localStorage.setItem("token", data.token);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
     return <div>
-            <div style={{
-                paddingTop: 150,
-                marginBottom: 10,
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <Typography variant={"h6"}>
+        <div style={{
+            paddingTop: 150,
+            marginBottom: 10,
+            display: "flex",
+            justifyContent: "center"
+        }}>
+            <Typography variant={"h6"}>
                 Welcome back. Sign in below
-                </Typography>
-            </div>
-        <div style={{display: "flex", justifyContent: "center"}}>
-            <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+            </Typography>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <Card varint={"outlined"} style={{ width: 400, padding: 20 }}>
                 <TextField
+                    onChange={(evant11) => {
+                        let elemt = evant11.target;
+                        setEmail(elemt.value);
+                    }}
                     fullWidth={true}
-                    id="outlined-basic"
                     label="Email"
                     variant="outlined"
                 />
-                <br/><br/>
+                <br /><br />
                 <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
                     fullWidth={true}
-                    id="outlined-basic"
                     label="Password"
                     variant="outlined"
                     type={"password"}
                 />
-                <br/><br/>
+                <br /><br />
 
-                <Button size={"large"} variant="contained"> Signin</Button>
+                <Button size={"large"} variant="contained" onClick={handleSignIn}> Signin</Button>
             </Card>
         </div>
     </div>
