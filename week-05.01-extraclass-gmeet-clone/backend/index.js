3760eef70bfe889675e0de0c24a6f80560ce2a65
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-11-24 07:34:26 +0530

    socket connection established

--- week-05.01-extraclass-gmeet-clone/backend/index.js
+++ week-05.01-extraclass-gmeet-clone/backend/index.js
@@ -10,7 +10,15 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    io.on('connection', (socket) => {
+        socket.on('chat message', (msg) => {
+          io.emit('chat message', msg);
+        });
+      });
 });
 
 server.listen(3000, () => {
