f64cd873450b3c83f87166db3ec2b1c5eb01bde3
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-12-02 22:34:44 +0530

    Update course selector in admin client

--- week-7/admin-client/src/store/selectors/course.js
+++ week-7/admin-client/src/store/selectors/course.js
@@ -2,11 +2,13 @@ import { userState } from "../atoms/user";
 import {selector} from "recoil";
 import { courseState } from "../atoms/course";
 
+// must define a selector for each recoil state
 export const isCourseLoading = selector({
+  //  this is a unique key to identify the seletor
   key: 'isCourseLoaingState',
+  // in ths get key:value pair we get the get callback func in which we must pass the atom state which we need to update.
   get: ({get}) => {
     const state = get(courseState);
-
     return state.isLoading;
   },
 });
