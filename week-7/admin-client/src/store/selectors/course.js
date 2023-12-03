e8fe03ec9b3ac0fba2eba0bccb0bd4b53777385a
SDE-ADNAN <adnansdeofficial@gmail.com>
2023-12-03 23:15:17 +0530

    Add selectors for course state

--- week-7/admin-client/src/store/selectors/course.js
+++ week-7/admin-client/src/store/selectors/course.js
@@ -2,6 +2,11 @@ import { userState } from "../atoms/user";
 import {selector} from "recoil";
 import { courseState } from "../atoms/course";
 
+// the concept of selectors is that you can have a recoil state with 
+// { email: abc@gmail.com, isLoading:true } two state as key of an object
+// when you use this state then if only email is changed  and your component is not using the email state and its only using the isloading state then also it will rerender , so to tacle this we use selectors  which can give you a separate state value from bigger state , and you can also use this selectors to combine 2-3 different states and return a complex state.
+
+
 // must define a selector for each recoil state
 export const isCourseLoading = selector({
   //  this is a unique key to identify the seletor
@@ -17,7 +22,6 @@ export const courseDetails = selector({
   key: 'courseDetailsState',
   get: ({get}) => {
     const state = get(courseState);
-
     return state.course;
   },
 });
